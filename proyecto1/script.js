7a267406fc4475d033ef4e095ef3d4a982218f3f
val132005 <141514145+val132005@users.noreply.github.com>
2023-10-22 13:06:22 -0500

    Revisión script.js

--- proyecto1/script.js
+++ proyecto1/script.js
@@ -1,60 +1,53 @@
-// .querySelector metodo para realizar a rreglos a partir de un contenid definido 
+// .querySelector metodo para realizar a rreglos a partir de un contenid definido
 
 const pantalla = document.querySelector(".pantalla");
 
 const botones = document.querySelectorAll(".boton");
 
-botones.forEach(boton =>{
-    boton.addEventListener("click", () => {
-        let botonOn = boton.textContent;
-        if (boton.id === "borrar"){
-            if (pantalla.textContent.length === 1 || pantalla.textContent === "!Erorr!"){
-                pantalla.textContent="0";
-            }else{
-                
-                pantalla.textContent = pantalla.textContent.slice(0, -1);
-                
-            }
-            return;
-            
-        }
+botones.forEach((boton) => {
+  boton.addEventListener("click", () => {
+    let botonOn = boton.textContent;
+    if (boton.id === "borrar") {
+      if (
+        pantalla.textContent.length === 1 ||
+        pantalla.textContent === "!Erorr!"
+      ) {
+        pantalla.textContent = "0";
+      } else {
+        pantalla.textContent = pantalla.textContent.slice(0, -1);
+      }
+      return;
+    }
 
-        if (boton.id === "igual") {
-            try {
-                const expresion = pantalla.textContent;
-                if (expresion.includes("//")) {
-                    pantalla.textContent = "!Erorr!";
-                } else {
-                    const resultado = eval(expresion);
-                    if (isNaN(resultado) || !isFinite(resultado)) {
-                        pantalla.textContent = "!Erorr!";
-                    } else {
-                        pantalla.textContent = resultado;
-                    }
-                }
-            } catch (error) {
-                pantalla.textContent = "!Erorr!";
-            }
-            return;
-        }
-
-
-        if (boton.id === "limpiar") {
-            pantalla.textContent = "0";
-            return;
-        }
-
-        if (pantalla.textContent === "0" || pantalla.textContent === "!Erorr!") {
-            pantalla.textContent = botonOn;
+    if (boton.id === "igual") {
+      try {
+        const expresion = pantalla.textContent;
+        if (expresion.includes("//")) {
+          pantalla.textContent = "!Erorr!";
         } else {
-            pantalla.textContent +=botonOn;
+          const resultado = eval(expresion);
+          if (isNaN(resultado) || !isFinite(resultado)) {
+            pantalla.textContent = "!Erorr!";
+          } else {
+            pantalla.textContent = resultado;
+          }
         }
+      } catch (error) {
+        pantalla.textContent = "!Erorr!";
+      }
+      return;
+    }
 
-        
-    })
+    if (boton.id === "limpiar") {
+      pantalla.textContent = "0";
+      return;
+    }
 
+    if (pantalla.textContent === "0" || pantalla.textContent === "!Erorr!") {
+      pantalla.textContent = botonOn;
+    } else {
+      pantalla.textContent += botonOn;
+    }
+  });
+});
 
-
-})
-
-})
